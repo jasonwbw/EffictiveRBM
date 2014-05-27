8d53cadeedbcdc064cf7d47d1eca37eaa24b6389
jasonwbw <384444165@163.com>
2014-05-27 15:03:43 +0800

    complete RBMLinear

--- rbm/autoencoder/rbm.py
+++ rbm/autoencoder/rbm.py
@@ -20,7 +20,7 @@ class RBM(object):
 	def train(self, visible_data, \
 		max_epochs = 50, batch = 10, \
 		initialmomentum = 0.5, finalmomentum = 0.9, \
-		weight_rate = 0.1, vbias_rate = 0.1, hbias_rate = 0.1, \
+		weight_rate = 0.01, vbias_rate = 0.01, hbias_rate = 0.01, \
 		weightcost = 0.0002):
 		grad_weight = zeros((len(self.visible_bias), len(self.hidden_bias)))
 		grad_hbias = zeros((1, len(self.hidden_bias)))
@@ -85,7 +85,58 @@ class RBM(object):
 
 
 class RBMLinear(RBM):
-	pass
+	def __init__(self, num_visible, num_hidden):
+		RBM.__init__(self, num_visible, num_hidden)
+
+	def train(self, visible_data, \
+		max_epochs = 50, batch = 10, \
+		initialmomentum = 0.5, finalmomentum = 0.9, \
+		weight_rate = 0.01, vbias_rate = 0.01, hbias_rate = 0.01, \
+		weightcost = 0.0002):
+		grad_weight = zeros((len(self.visible_bias), len(self.hidden_bias)))
+		grad_hbias = zeros((1, len(self.hidden_bias)))
+		grad_vbias = zeros((1, len(self.visible_bias)))
+		for epoch in xrange(max_epochs):
+			error = 0.
+			for start in xrange(batch):
+				if start % 10 == 0:
+					print "epoch %d, batch %d" % (epoch, start + 1)
+				data = visible_data[start::batch]
+				pos_hidden_activations = dot(data, self.weights) + self.hidden_bias
+				pos_hidden_probs = pos_hidden_activations
+				pos_hidden_states = pos_hidden_probs + random.randn(len(data), len(self.hidden_bias))
+				posprods = dot(data.T, pos_hidden_probs)
+				pos_hidden_act = sum(pos_hidden_probs)
+				pos_visible_act = sum(data)
+
+				neg_visible_activations = dot(pos_hidden_states, self.weights.T) + self.visible_bias
+				neg_visible_probs = self._sigmoid(neg_visible_activations)
+				neg_hidden_activations = dot(neg_visible_probs, self.weights) + self.hidden_bias
+				neg_hidden_probs = neg_hidden_activations
+				negprods = dot(neg_visible_probs.T, neg_hidden_probs)
+				neg_hidden_act = sum(neg_hidden_probs)
+				neg_visible_act = sum(neg_visible_probs)
+				error += sum((data - neg_visible_probs) ** 2)
+				
+				if epoch > 5:
+					momentum = finalmomentum
+				else:
+					momentum = initialmomentum
+				
+				if epoch == max_epochs - 1:
+					if len(self.hidden_probs) == 0:
+						self.hidden_probs = neg_hidden_probs
+					else:
+						self.hidden_probs = concatenate((self.hidden_probs, neg_hidden_probs))
+
+				grad_weight = momentum * grad_weight + weight_rate * ((posprods - negprods) / len(data) - weightcost * self.weights)
+				grad_vbias = momentum * grad_vbias + vbias_rate * (pos_visible_act - neg_visible_act) / len(data)
+				grad_hbias = momentum * grad_hbias + hbias_rate * (pos_hidden_act - neg_hidden_act) / len(data)
+				
+				self.weights += grad_weight
+				self.visible_bias += grad_vbias
+				self.hidden_bias += grad_hbias
+			print "epoch %d, error %d\n" % (epoch, error)
 #endclass RBMLinear
 
 if __name__ == '__main__':
