99ed55c7bbab36cd148143a9b85dcdb83833db9a
jasonwbw <384444165@163.com>
2014-05-31 20:10:39 +0800

    delete some object to save memory

--- rbm/autoencoder/mnist_deep_auto.py
+++ rbm/autoencoder/mnist_deep_auto.py
@@ -16,16 +16,13 @@ import pickle
 
 class MNISTDeepAuto(object):
 
-
 	def __init__(self, batch_num = 100):
 		self._load_data()
-
 
 	def _load_data(self):
 		print "begin converting data into memory"
 		self.converter = Converter()
 		print "converting end\n"
-
 
 	def train(self):
 		print "train rbm level 1"
@@ -36,6 +33,7 @@ class MNISTDeepAuto(object):
 		self.pickle_dumps(rbm.hidden_bias, 'l1_hb.pkl')
 		self.pickle_dumps(rbm.visible_bias, 'l1_vb.pkl')
 		del rbm
+		del self.converter
 		print "train rbm level 1 end\n"
 		
 		print "train rbm level 2"
@@ -46,6 +44,7 @@ class MNISTDeepAuto(object):
 		self.pickle_dumps(rbm_l2.hidden_bias, 'l2_hb.pkl')
 		self.pickle_dumps(rbm_l2.visible_bias, 'l2_vb.pkl')
 		del rbm_l2
+		del hidden_probs1
 		print "train rbm level 2 end\n"
 
 		print "train rbm level 3"
@@ -56,6 +55,7 @@ class MNISTDeepAuto(object):
 		self.pickle_dumps(rbm_l3.hidden_bias, 'l3_hb.pkl')
 		self.pickle_dumps(rbm_l3.visible_bias, 'l3_vb.pkl')
 		del rbm_l3
+		del hidden_probs2
 		print "train rbm level 3 end\n"
 
 		print "train rbm level 4"
@@ -66,6 +66,7 @@ class MNISTDeepAuto(object):
 		self.pickle_dumps(rbm_l4.hidden_bias, 'l4_hb.pkl')
 		self.pickle_dumps(rbm_l4.visible_bias, 'l4_vb.pkl')
 		del rbm_l4
+		del hidden_probs3
 		print "train rbm level 4 end\n"
 
 	def pickle_dumps(self, obj, filename):
