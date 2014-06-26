9c071f6a36a27d2f0888eba690bb500165818a42
jasonwbw <384444165@163.com>
2014-06-26 11:44:19 +0800

    add a file batch iterator

--- rbm/autoencoder/batch_iterator.py
+++ rbm/autoencoder/batch_iterator.py
@@ -4,6 +4,7 @@
 # Author : Jasonwbw@yahoo.com
 
 from abc import ABCMeta, abstractmethod
+import numpy as np
 
 class BatchIterator(object):
 
@@ -43,5 +44,44 @@ class DefaultBatchIterator(BatchIterator):
 		return self.data[self.current_batch : : self.batch]
 #endclass DefaultBatchIterator
 
+class FileBatchIterator(BatchIterator):
+
+	def _file_length(self, filename):
+		count = 0
+		with open(filename, 'r') as fp:
+			for line in fp:
+				count += 1
+		return count
+
+	def __init__(self, batch, filename):
+		BatchIterator.__init__(self, batch)
+		self.filename = filename
+		self.file_length = self._file_length(filename)
+		self._f = open(filename, 'r')
+		if self.file_length % batch == 0:
+			self.one_batch_line = self.file_length / batch
+		else:
+			self.one_batch_line = self.file_length / batch + 1
+
+	def next_batch(self):
+		if self.current_batch >= self.batch - 1:
+			return None
+		self.current_batch += 1
+		data = []
+		for i in xrange(min(self.one_batch_line, self.file_length - self.current_batch * self.one_batch_line)):
+			data.append(self._f.readline().strip())
+		return np.array(data)
+
+	def back2start(self):
+		BatchIterator.back2start(self)
+		self._f.close()
+		self._f = open(self.filename, 'r')
+#endclass FileBatchIterator
+
 if __name__ == '__main__':
-	converter = Converter()
+	fbi = FileBatchIterator(5, 'backprop.py')
+	print len(fbi.next())
+	print len(fbi.next())
+	print len(fbi.next())
+	print len(fbi.next())
+	print len(fbi.next())
